cd30b912200048511712e18ad3e0e8be2165e449
Alex Bonwit <alexbonwit@mac.com>
2019-11-15 18:28:01 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,16 +1,29 @@
-def roll_call_dwarves# code an argument here
-  # Your code here
+def roll_call_dwarves(dwarves)
+  dwarves.each_with_index do |item, index|
+    puts "#{index + 1}. #{item}"
+  end 
 end
 
-def summon_captain_planet# code an argument here
-  # Your code here
+def summon_captain_planet(calls)
+  calls.collect do |string|
+    string.capitalize + "!"
+  end
 end
 
-def long_planeteer_calls# code an argument here
-  # Your code here
+def long_planeteer_calls(calls)
+  calls.any? { |word| word.length > 4}
 end
 
-def find_the_cheese# code an argument here
-  # the array below is here to help
+def find_the_cheese(strarray)
+  results = nil
   cheese_types = ["cheddar", "gouda", "camembert"]
+  strarray.map.with_index do |string, index|
+    if strarray.include?(cheese_types)
+      # results = true
+      return string[index]
+    end
+  end
+  if results == false
+    return nil
+  end
 end
